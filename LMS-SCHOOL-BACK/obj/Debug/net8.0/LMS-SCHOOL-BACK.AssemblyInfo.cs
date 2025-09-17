f721d627cfa748cd2e04549adb014580c6e9585c
DBSELR <89074505+DBSELR@users.noreply.github.com>
2025-09-17 18:06:24 +0530

    Stop tracking bin/ and obj/; add .gitignore

--- LMS-SCHOOL-BACK/obj/Debug/net8.0/LMS-SCHOOL-BACK.AssemblyInfo.cs
+++ LMS-SCHOOL-BACK/obj/Debug/net8.0/LMS-SCHOOL-BACK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS-SCHOOL-BACK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f5604ea2a7765fadd666ab61f3a17056b9760f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f160b26bab0422f90a9f4928e5b9000bfe0785")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS-SCHOOL-BACK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS-SCHOOL-BACK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LMS-SCHOOL-BACK/obj/Release/net8.0/LMS-SCHOOL-BACK.AssemblyInfo.cs
+++ LMS-SCHOOL-BACK/obj/Release/net8.0/LMS-SCHOOL-BACK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS-SCHOOL-BACK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94b702f6c0257d8f3eed6d36806a14544db8185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f160b26bab0422f90a9f4928e5b9000bfe0785")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS-SCHOOL-BACK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS-SCHOOL-BACK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
